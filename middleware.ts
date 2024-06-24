4e6000a8d74d5cb16852355e7799972517998efd
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-06-24 23:01:06 +0700

    add auth

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,14 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher, redirectToSignIn } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+const isPublicRoute = createRouteMatcher(['/api/uploadthing', '/sign-in(.*)', '/sign-up(.*)']);
 
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req) => {
+    if (!isPublicRoute(req)) {
+      auth().protect(); // Protect the route if it matches the defined criteria
+    }
+});
+
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
